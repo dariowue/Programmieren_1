76250e9506ff64a0cc083c884752a8391a642d53
DarioWueest <wuuest.dario@gmail.com>
2025-09-29 12:54:15 +0200

    Finish Aufgabe_06

--- Aufgabe_06/Program.cs
+++ Aufgabe_06/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        int linebreak = 10;
+        for (int i = 1; i < 11; ++i)
+        {
+            for (int n = 1; n < 11; n++)
+            {
+                int result = i * n;
+                Console.Write(result + "\t");
+                if (linebreak == result)
+                {
+                    Console.WriteLine("  ");
+                    linebreak = linebreak + 10;
+                }
+            }
+        }
+
     }
 }
