1bd1e0009589267e5688cda49479f339fcf7ae6c
DarioWueest <wuuest.dario@gmail.com>
2025-09-29 14:55:40 +0200

    Finish Aufgabe_08 and add comment

--- Aufgabe_08/Program.cs
+++ Aufgabe_08/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        string[] forbiddenWords = { "viagra", "sex", "porno", "fick", "schlampe", "arsch" };
+        Console.WriteLine("Your comment:");
+        string comment= Console.ReadLine();
+        string finalComment = comment.ToLower(); // Hier wird die Eingabe des Kommentars klein geschrieben, egal wie es eingegeben wurde.
+        
+        int amountforbiddenWords = 0;
+        
+
+        foreach (string word in forbiddenWords)
+        {
+            if ( finalComment.Contains(word))
+            {
+                amountforbiddenWords++;
+            }
+            
+        }
+        
+        Console.WriteLine($"You comment includes {amountforbiddenWords} forbidden words");
+        Console.WriteLine("Your comment won't be posted!");
+        
     }
+    
+    
 }
