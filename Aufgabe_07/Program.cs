989dbf4c8d0108ff0a5c98baa627acb0fe87dab6
DarioWueest <wuuest.dario@gmail.com>
2025-09-29 13:13:48 +0200

    Finish Aufgabe_07

--- Aufgabe_07/Program.cs
+++ Aufgabe_07/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Numbers between 1 and 30 that can be divided by 5 and/or 3:");
+
+        int i = 1;
+
+        while (i <= 30)
+        {
+            i++;
+            if (i % 5 == 0 || i % 3 == 0)
+            {
+                if (i == 30)
+                {
+                    Console.Write(i);
+                }
+                else
+                {
+                    Console.Write(i + ","); 
+                }
+            }
+        }
     }
 }
