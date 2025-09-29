7fc5ef75537212912c21b8f0dde74d3b36b321c0
DarioWueest <wuuest.dario@gmail.com>
2025-09-29 11:07:59 +0200

    Finish Aufgabe_05

--- Aufgabe_05/Program.cs
+++ Aufgabe_05/Program.cs
@@ -1,9 +1,43 @@
-﻿namespace Aufgabe_05;
+﻿using System.ComponentModel.Design;
+
+namespace Aufgabe_05;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        double numberofKilometers;
+        double numberofLaps;
+        string input;
+        
+
+        Console.Write("Enter the number of kilometers: ");
+        numberofKilometers = Convert.ToDouble(Console.ReadLine());
+
+        if (numberofKilometers > 42)
+        {
+            Console.WriteLine("You can't do it!");
+        }
+        else
+        {
+            numberofLaps = numberofKilometers / 0.4;
+            Console.WriteLine($"That's {numberofLaps} rounds. Ready for the run? yes or no?");
+            input = Console.ReadLine();
+
+            if (input == "yes")
+            {
+               int i = 1;
+               while (i <= numberofLaps)
+               {
+                   Console.WriteLine($"You run lap {i} ");
+                   i++;
+               }
+
+               if (i > numberofLaps)
+               {
+                   Console.WriteLine("You did it!");
+               }
+            }
+        }
     }
 }
