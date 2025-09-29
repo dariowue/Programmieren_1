70f46044c682b632ff6e52a9a8996742a77e5c12
DarioWueest <wuuest.dario@gmail.com>
2025-09-29 08:24:13 +0200

    Finish Aufgabe_01

--- Aufgabe_01/Program.cs
+++ Aufgabe_01/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        int number1;
+        int number2; 
+        
+        Console.Write("Number 1: ");
+        number1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Number 2: ");
+        number2 = Convert.ToInt32(Console.ReadLine());
+        
+        Console.WriteLine($"sum = {number1 + number2}");
     }
 }
