794487dd91c6f97df954f9daa76ffaa8cb653cb0
DarioWueest <wuuest.dario@gmail.com>
2025-09-29 10:26:46 +0200

    Finish Aufgabe_04

--- Aufgabe_04/Program.cs
+++ Aufgabe_04/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        string[] months = {"","January", "February", "March",  "April", "May", "June", "July", "August", "September", "October", "November", "December"};
+        string input;
+        int monthNumber = 0;
+         
+        Console.Write("Enter a number between 1-12: ");
+        input = Console.ReadLine();
+
+        if (int.TryParse(input, out monthNumber) == true)
+        {
+            if (monthNumber > 1 && monthNumber < 12)
+            {
+                Console.WriteLine(months[monthNumber]);
+            }
+            else
+            {
+                Console.WriteLine("Invalid Input. Enter a number between 1-12.");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Invalid input. Enter an Integer");
+        }
     }
 }
