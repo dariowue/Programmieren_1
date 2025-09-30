ca6269dd0abc87a824228abfa1914e31a412c5a6
DarioWueest <wuuest.dario@gmail.com>
2025-09-30 08:50:53 +0200

    Finish Aufgabe_10

--- Aufgabe_10/Program.cs
+++ Aufgabe_10/Program.cs
@@ -4,6 +4,32 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        int userNumber;
+        int finalResult;
+        
+        Console.Write("number: ");
+        userNumber = Convert.ToInt32(Console.ReadLine());
+        
+        finalResult = CalculateChecksum(userNumber);
+        Console.WriteLine(finalResult);
+        
+        
+        
     }
+    
+    static int CalculateChecksum(int userNumber)
+    {
+        int checksum = 0;
+        int sum = 0;
+        
+        while (userNumber != 0)
+        {
+            sum = sum + (userNumber % 10);
+            userNumber /= 10;
+        }
+
+        return sum;
+        
+    }
+    
 }
