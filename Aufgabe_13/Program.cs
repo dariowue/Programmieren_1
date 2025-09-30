4c6f39a0fcfe9afdf8009fa227b071cace3e613b
DarioWueest <wuuest.dario@gmail.com>
2025-09-30 12:52:51 +0200

    Finish Aufgabe_13

--- Aufgabe_13/Program.cs
+++ Aufgabe_13/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Enter the length of the line: ");
+        Console.WriteLine(" ");
+        string input = Console.ReadLine();
+        int intInput = Convert.ToInt32(input);
+
+        string [,] stringArray = new string [intInput, intInput];
+        
+        for (int i = 0; i < intInput; i++)
+        {
+            stringArray[i,i] = " ";
+        }
+
+        for (int row = 0; row < stringArray.GetLength(0); row++)
+        {
+            for (int column = 0; column < stringArray.GetLength(1); column++)
+            {
+                Console.Write(stringArray[row, column] + "*");
+            }
+            Console.WriteLine();
+        }
+
     }
+        
 }
