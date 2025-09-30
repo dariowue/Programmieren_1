9d857c897b60aad59e806cd87dc51e8953d3bd21
DarioWueest <wuuest.dario@gmail.com>
2025-09-30 11:16:27 +0200

    Finish Aufgabe_12

--- Aufgabe_12/Program.cs
+++ Aufgabe_12/Program.cs
@@ -4,6 +4,34 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("-------------------");
+        Console.WriteLine("Zahlen Aufsummieren");
+        Console.WriteLine("-------------------");
+        Console.WriteLine("\nEnter the integers to be summed, seperated by commas");
+        
+        string input = Console.ReadLine();
+        string[] inputArray  = input.Split(',');
+        int [] intArray = Array.ConvertAll(inputArray, int.Parse);
+        int sum = 0;
+
+        for (int i = 0; i < intArray.Length; i++)
+        {
+            while (i <= intArray.Length - 1)
+            {
+                sum = intArray[i] + sum;
+                
+                if(i != intArray.Length -1)
+                {
+                    Console.Write("[" + i + "]" + " -> " + sum + ", ");
+                    i++;
+                }
+                else
+                {
+                    Console.Write("[" + i + "]" + " -> " + sum);
+                    i++;
+                }
+            }
+        }
     }
+    
 }
