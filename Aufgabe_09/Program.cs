acbc304250c18b90891f68992600dfb1ff75d9a9
DarioWueest <wuuest.dario@gmail.com>
2025-09-30 08:27:13 +0200

    Finish Aufgabe_09

--- Aufgabe_09/Program.cs
+++ Aufgabe_09/Program.cs
@@ -4,6 +4,49 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        
+        bool keypressed = false;
+
+        while (keypressed == false)
+        {
+            string bin = (" ");
+            string input;
+            int userNumber;
+            int rest;
+            int value;
+
+            Console.WriteLine("integer number (q + enter to Quit)");
+            input = Console.ReadLine();
+            
+            
+            
+            if (input == "q")
+            {
+                keypressed = true; 
+                Console.WriteLine("You pressed q which stopped the programm");
+            }
+            else
+            {
+                userNumber = Convert.ToInt32(input);
+                keypressed = false;
+                while (userNumber != 0)
+                {
+                    rest = userNumber % 2;
+                    bin = rest + bin;
+                    value = userNumber / 2;
+                    userNumber = value;
+                }
+
+                if (userNumber == 0) ;
+                {
+                    Console.WriteLine(bin);
+                }
+            }
+        }
+
+        
+
+
+
     }
 }
