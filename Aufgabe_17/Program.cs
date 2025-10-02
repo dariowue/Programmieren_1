355309ba0d43b4a69419ea50b6939cadb96dfcc0
DarioWueest <wuuest.dario@gmail.com>
2025-10-02 07:54:38 +0200

    Finish Aufgabe_17

--- Aufgabe_17/Program.cs
+++ Aufgabe_17/Program.cs
@@ -4,6 +4,35 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.Write("Enter your birthday: ");
+        DateTime birthday = Convert.ToDateTime(Console.ReadLine());
+        DateTime today = DateTime.Today;
+        
+        int ageYears = today.Year - birthday.Year;
+        int remainingMonths = today.Month - birthday.Month;
+        int remainingDays = today.Day - birthday.Day;
+        
+        if (remainingDays < 0)
+        {
+            DateTime previousMonth = today.AddMonths(-1);
+            int daysInPreviousMonth = DateTime.DaysInMonth(previousMonth.Year, previousMonth.Month);
+            remainingDays += daysInPreviousMonth;
+            remainingMonths--; 
+        }
+        
+        if (remainingMonths < 0)
+        {
+            remainingMonths += 12;
+            ageYears--;
+        }
+        
+        int totalDays = (today - birthday).Days;
+        int totalWeeks = totalDays / 7;
+        int totalMonths = ageYears * 12 + remainingMonths;
+        
+        Console.WriteLine($"Years: {ageYears}");
+        Console.WriteLine($"Months: {remainingMonths}");
+        Console.WriteLine($"Weeks: {totalWeeks}");
+        Console.WriteLine($"Days: {totalDays}");
     }
 }
