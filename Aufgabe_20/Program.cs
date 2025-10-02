5ccd44fa632680f88f1d0c9676d701c9c5e539fe
DarioWueest <wuuest.dario@gmail.com>
2025-10-02 15:21:01 +0200

    Finish Aufgabe_20

--- Aufgabe_20/Program.cs
+++ Aufgabe_20/Program.cs
@@ -1,9 +1,60 @@
-﻿namespace Aufgabe_20;
+﻿using System;
+using System.IO;
+using System.Net;
+using Newtonsoft.Json.Linq;
 
-class Program
+namespace Aufgabe_20
 {
-    static void Main(string[] args)
+    class Program
     {
-        Console.WriteLine("Hello, World!");
+        static bool restart = true;
+        static void Main(string[] args)
+        {
+            while (restart == true)
+            {
+                Console.WriteLine("This is a Joke IPA:");
+
+                WebRequest request = WebRequest.Create("https://witzapi.de/api/joke/");
+                WebResponse response = request.GetResponse();
+
+                using Stream responseStream = response.GetResponseStream();
+                using StreamReader reader = new StreamReader(responseStream);
+                string json = reader.ReadToEnd();
+
+                JArray array = JArray.Parse(json);
+
+                foreach (var joke in array)
+                {
+                    Console.ResetColor();
+                    Console.WriteLine(joke["text"]);
+                    Console.Write("Do you want to hear another one? y/n \n\n");
+                    CheckKey();
+                }
+            }
+        }
+        
+        static void CheckKey()
+        { 
+            ConsoleKeyInfo keyInfo = Console.ReadKey(true);
+            if (keyInfo.Key == ConsoleKey.Y)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                restart = true;
+            }
+            else if (keyInfo.Key == ConsoleKey.N)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("\n\nToo bad, this one would have been a good one");
+                restart = false;
+            }
+            else
+            {
+                Console.Clear();
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("This wasn't an option, yet you still chose to press it");
+                restart = false;
+            }
+        }
+        
     }
 }
