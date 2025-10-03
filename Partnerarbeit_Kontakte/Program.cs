067cb5b1698e9780215558b2e39401c713709d86
DarioWueest <wuuest.dario@gmail.com>
2025-10-03 13:07:44 +0200

    Add comments for better understanding

--- Partnerarbeit_Kontakte/Program.cs
+++ Partnerarbeit_Kontakte/Program.cs
@@ -7,7 +7,7 @@ class Program
 {
     static bool kontaktAbfrage = true;
     static bool ReihenfolgeVorundNachname;
-    static Dictionary<int, Kontakt> kontakte = new Dictionary<int, Kontakt>();
+    static Dictionary<int, Kontakt> kontakte = new Dictionary<int, Kontakt>(); //Muss hier sein, sonst kann man nicht auf die gespeicherten Werte zugreifen.
 
     static void Main(string[] args)
     {
@@ -17,52 +17,54 @@ class Program
 
         KontakteHinzufuegen();
 
-        if (kontaktAbfrage == false)
+        if (kontaktAbfrage == false) //falls Eingabe beendet
         {
             Console.Clear();
             Console.Write("Wie sollen die Kontakte ausgegeben werden? \nVor- oder Nachname zuerst? (v/n)");
-            CheckKeyVoroderNachname();
+            CheckKeyVoroderNachname(); //Reihenfolge lesen
 
             Console.WriteLine("\n--- Kontakte ---");
-            foreach (var eintrag in kontakte)
+            foreach (var eintrag in kontakte) //Für jeden Eintrag (Person) in Kontakte
             {
-                var person = eintrag.Value;
+                var person = eintrag.Value; //Variable Person wird Wert aus Kontakt Dicitonary zugeordnet
 
-                if (ReihenfolgeVorundNachname == true)
+                if (ReihenfolgeVorundNachname == true) //true: Beginn mit Vornamen
                 {
                     Console.WriteLine($"{person.Vorname} {person.Nachname} ({person.EMail})");
                 }
-                else if (ReihenfolgeVorundNachname == false)
+                else if (ReihenfolgeVorundNachname == false) //false: Beginn mit Nachnamen
                 {
                     Console.WriteLine($"{person.Nachname} {person.Vorname} ({person.EMail})");
                 }
             }
         }
+    }
 
         static void KontakteHinzufuegen()
         {
-            while (kontaktAbfrage == true)
+            while (kontaktAbfrage == true) //Um abbrechen zu können, falls j gedrückt
             {
-                Kontakt person = new Kontakt();
+                Kontakt person = new Kontakt(); //neues Element im Dictionary erstellen
 
                 Console.Write("\n\nVorname: ");
-                person.Vorname = Console.ReadLine();
+                person.Vorname = Console.ReadLine(); //Werte den Unterkategorien der Klasse zuweisen
 
                 Console.Write("Nachname: ");
-                person.Nachname = Console.ReadLine();
+                person.Nachname = Console.ReadLine(); //Werte den Unterkategorien der Klasse zuweisen
 
                 Console.Write("EMail: ");
-                person.EMail = Console.ReadLine();
+                person.EMail = Console.ReadLine(); //Werte den Unterkategorien der Klasse zuweisen
 
-                kontakte.Add(kontakte.Keys.Count + 1, person);
+                kontakte.Add(kontakte.Keys.Count + 1, person); //Der Inhalt des Dictionary wird um 1 erhöht und dann unter diesem Key werden die Werte gespeichert
 
                 Console.Write("Eingabe beenden? (j/n) ");
-                CheckKeyJaoderNein();
+                CheckKeyJaoderNein(); //Soll die Erfassung weiter gehen?
             }
         }
 
         static void CheckKeyJaoderNein()
         {
+            //Frage: Abbrechen? Ja -> Abbruch / Nein -> While-Schlaufe wiederholt sich erneut
             ConsoleKeyInfo keyInfo = Console.ReadKey(true);
             if (keyInfo.Key == ConsoleKey.J)
             {
@@ -80,6 +82,7 @@ class Program
 
         static void CheckKeyVoroderNachname()
         {
+            //V: Beginnt mit Vorname, N: Beginnt mit Nachname
             ConsoleKeyInfo keyInfo = Console.ReadKey(true);
             if (keyInfo.Key == ConsoleKey.V)
             {
@@ -98,4 +101,3 @@ class Program
         }
 
     }
-}
